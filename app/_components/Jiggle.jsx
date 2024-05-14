e51da318e196b2f68b870f56043f094c4ad1a4da
Jaskaran1519 <jaskaran1519@gmail.com>
2024-05-14 20:47:43 +0530

    okok

--- app/_components/Jiggle.jsx
+++ app/_components/Jiggle.jsx
@@ -1,38 +1,34 @@
-import { useRef, useState } from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
-export default function Framer({ children }) {
-  const ref = useRef(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+export default function index({ children }) {
+  const magnetic = useRef(null);
 
-  const handleMouse = (e) => {
-    const { clientX, clientY } = e;
-    const { height, width, left, top } = ref.current.getBoundingClientRect();
-    const middleX = clientX - (left + width / 2);
-    const middleY = clientY - (top + height / 2);
-    setPosition({ x: middleX, y: middleY });
-  };
+  useEffect(() => {
+    console.log(children);
+    const xTo = gsap.quickTo(magnetic.current, "x", {
+      duration: 1,
+      ease: "elastic.out(1, 0.3)",
+    });
+    const yTo = gsap.quickTo(magnetic.current, "y", {
+      duration: 1,
+      ease: "elastic.out(1, 0.3)",
+    });
 
-  const reset = () => {
-    setPosition({ x: 0, y: 0 });
-  };
+    magnetic.current.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      const { height, width, left, top } =
+        magnetic.current.getBoundingClientRect();
+      const x = clientX - (left + width / 2);
+      const y = clientY - (top + height / 2);
+      xTo(x * 0.35);
+      yTo(y * 0.35);
+    });
+    magnetic.current.addEventListener("mouseleave", (e) => {
+      xTo(0);
+      yTo(0);
+    });
+  }, []);
 
-  const { x, y } = position;
-  return (
-    <motion.div
-      style={{ position: "relative" }}
-      ref={ref}
-      onMouseMove={handleMouse}
-      onMouseLeave={reset}
-      animate={{ x, y }}
-      transition={{
-        type: "spring",
-        stiffness: 300,
-        damping: 5,
-        mass: 0.1,
-      }}
-    >
-      {children}
-    </motion.div>
-  );
+  return React.cloneElement(children, { ref: magnetic });
 }
